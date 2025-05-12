4d7606ab9887c4442de7d1610cb52411ad9288c2
youphendriks <youp_hendriks@hotmail.com>
2025-05-12 15:58:43 +0200

    Working mongoDB

--- pages/2-stats.py
+++ pages/2-stats.py
@@ -1,12 +1,25 @@
+# streamlit_app.py
+
 import streamlit as st
-import numpy as np
-import pandas as pd
+import pymongo
+from pymongo.mongo_client import MongoClient
+from pymongo.server_api import ServerApi
 
-container1 = st.container(border=True)
-container1.header("Overall", divider="gray")
+uri ="mongodb+srv://"+st.secrets.mongo.username+":"+st.secrets.mongo.password+"@tmm-ll.6siai.mongodb.net/?retryWrites=true&w=majority&appName=TMM-LL"
+# Create a new client and connect to the server
+client = MongoClient(uri, server_api=ServerApi('1'))
 
-container2 = st.container(border=True)
-container2.header("Player", divider="gray")
-df = pd.DataFrame(np.random.randn(50, 3), columns=("col %d" % i for i in range(3)))
-container2.dataframe(df)
+# Pull data from the collection.
+# Uses st.cache_data to only rerun when the query changes or after 10 min.
+@st.cache_data(ttl=600)
+def get_data():
+    db = client.TMMDB
+    items = db.TMM1.find()
+    items = list(items)  # make hashable for st.cache_data
+    return items
 
+items = get_data()
+
+# Print results.
+for item in items:
+    st.write(f"{item['P1']} played {item['P1deck']} versus {item['P2']} who played {item['P2deck']}.")
